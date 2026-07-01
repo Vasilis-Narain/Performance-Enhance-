9e533b9966d7038d6f5b51fe53dc13db2a50b8ec
vasilis <vasilis.narain@hotmail.it>
2026-07-01 19:33:23 +0200

    feat: figured out how to get random numbers in zig.

--- haversine_processor/src/main.zig
+++ haversine_processor/src/main.zig
@@ -3,10 +3,8 @@ const Io = std.Io;
 
 const haversine_processor = @import("haversine_processor");
 
-// NOTE(vasilis):
-//      CLI level API:
-//          executable -generate [uniform/cluster] [random seed] [number of coordinate pairs to generate]
-//
+/// CLI level API:
+///     `-generate [uniform/cluster] [random seed] [number of coordinate pairs to generate]`
 pub fn main(init: std.process.Init) !void {
     // This is appropriate for anything that lives as long as the process.
     const arena: std.mem.Allocator = init.arena.allocator();
@@ -16,14 +14,22 @@ pub fn main(init: std.process.Init) !void {
     var seed: u32 = undefined;
     var n: u32 = undefined;
 
-    // TODO(vasilis): silly me forgot that args are strings..
     if (args.len != 5 or !std.mem.eql(u8, args[1], "-generate")) {
         std.debug.print("Usage: -generate [uniform/cluster] [random seed] [number of coordinate pairs to generate]\n", .{});
         return;
     } else {
-        uniform = args[2];
-        seed = args[3];
-        n = args[4];
+        uniform = blk: {
+            if (std.mem.eql(u8, args[2], "uniform")) {
+                break :blk true;
+            } else if (std.mem.eql(u8, args[2], "cluster")) {
+                break :blk false;
+            } else {
+                std.debug.print("Usage: -generate [uniform/cluster] [random seed] [number of coordinate pairs to generate]\n", .{});
+                return;
+            }
+        };
+        seed = cstring_to_int(args[3]);
+        n = cstring_to_int(args[4]);
     }
 
     // In order to do I/O operations need an `Io` instance.
@@ -33,21 +39,35 @@ pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
-    try stdout_writer.print("Hello World\n", .{});
 
+    // TODO(vasilis): do the thing
     try haversine_processor.generateInput(stdout_writer, uniform, seed, n);
 
-    {
+    if (false) {
         // TODO: output the thing
         var outFile = try Io.Dir.cwd().createFile(io, "input/haversine_input.json", .{});
         defer outFile.close(io);
         var fileout_buffer: [1024]u8 = undefined;
         var fileout_writer = outFile.writer(io, &fileout_buffer);
         const file_writer = &fileout_writer.interface;
-        try file_writer.writeAll(); // TODO: put what we're writing :D
+        try file_writer.writeAll(); // TODO: might be wise to pass what we're writing as an argument :D
         try file_writer.flush();
     }
 
     // FLUSHING!
     try stdout_writer.flush(); // Don't forget to flush!
+}
+
+/// [NOTE(vasilis)]: this is not safe. It assumes you pass in a string that is a valid number.
+/// Why? All my homies hate strings that's why.
+fn cstring_to_int(str: [:0]const u8) u32 {
+    var result: u32 = 0;
+    var len: u8 = 0; // 255 digits are enough to encode a u32...
+    while (str[len] != 0) : (len += 1) {}
+    var i: u8 = 0;
+    while (i < len) : (i += 1) {
+        const current = str[i] - 48;
+        result += @intCast(current * std.math.pow(u32, 10, @intCast(len - (i + 1))));
+    }
+    return result;
 }
--- haversine_processor/src/root.zig
+++ haversine_processor/src/root.zig
@@ -2,19 +2,26 @@ const std = @import("std");
 const hs = @import("haversine.zig");
 const Io = std.Io;
 
-// TODO(vasilis): do the thing
-// the Thing: { "pairs" : [
-//              {"x0": 3.0, "y0": 7.0, "x1": ... , "y1": ... },
-//              {...},
-//              {...} //notice no end comma!
-//          ]}
-//
-// CLI level API:
-//  executable -generate [uniform/cluster] [random seed] [number of coordinate pairs to generate]
+/// [TODO(vasilis)]: do the Thing
+/// the Thing:
+/// `{ "pairs" : [
+///     {"x0": 3.0, "y0": 7.0, "x1": 11.879834 , "y1": 18.23424109 },
+///     {...},
+///     {...} //notice no end comma!
+/// ]}`
 pub fn generateInput(writer: *Io.Writer, uniform: bool, seed: u32, n: u32) Io.Writer.Error!void {
-    try writer.print("Here, we do THING\n", .{});
+    // This is how we do random numbers in zig.
+    // NOTE(vasilis): Try to remember how i figured this out as
+    // this pattern is very common in zig stdlib.
+    var prng: std.Random.DefaultPrng = .init(seed);
+    const rand = prng.random();
+    const random_number = rand.intRangeAtMost(i32, -180, 180);
 
-    _ = uniform;
-    _ = seed;
-    _ = n;
+    try writer.print(
+        \\uniform: {any}
+        \\seed: {d}
+        \\n: {d}
+        \\random_number: {d}
+        \\
+    , .{ uniform, seed, n, random_number });
 }
